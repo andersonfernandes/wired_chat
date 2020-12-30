2566867b9fd00e996a08aded5a03f69e7ef4da2a
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-12-30 16:25:31 -0300

    Removed rails-ujs deps
    - updated views that used rails-ujs
    - updated turbo-rails version
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,7 @@
 // that code so it'll be compiled.
 
 import "@hotwired/turbo-rails"
-import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
 ActiveStorage.start()
