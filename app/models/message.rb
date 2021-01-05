8e7b104dbd6253d59f1d39b0fb52747e9f1c7d5b
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-01-05 00:38:30 -0300

    Added scopped broadcast to chat messages
    - updated message model
    - updated chat and message views
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/models/message.rb
+++ app/models/message.rb
@@ -26,7 +26,7 @@ class Message < ApplicationRecord
 
   validates :text, :creator, presence: true
 
-  after_create_commit { broadcast_append_to 'messages' }
-  after_update_commit { broadcast_replace_to 'messages' }
-  after_destroy_commit { broadcast_remove_to 'messages' }
+  after_create_commit { broadcast_append_to chat, :messages }
+  after_update_commit { broadcast_replace_to chat, :messages }
+  after_destroy_commit { broadcast_remove_to chat, :messages }
 end
