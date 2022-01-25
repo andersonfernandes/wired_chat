9f9fbe50599396860268b3e5a13e2c1b77c6bb0f
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-01-24 23:30:17 -0300

    Improve docker setup and run
    Signed-off-by: Anderson Fernandes <anderson.fernandes@streetbees.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,4 +7,5 @@ yarn install
 [ -e tmp/pids/server.pid ] && rm tmp/pids/server.pid
 
 bundle exec rails db:prepare
-bundle exec rails server -p 5000 -b 0.0.0.0
+bundle exec rails server -p 5000 -b 0.0.0.0 &
+./bin/webpack-dev-server --hot
