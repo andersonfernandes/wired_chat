4731b6ae4f01dd3c8e4904bb14bca1efc0d8535e
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-02-04 23:54:33 -0300

    Revert webpacker dev server config
    Signed-off-by: Anderson Fernandes <anderson.fernandes@streetbees.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,5 +7,4 @@ yarn install
 [ -e tmp/pids/server.pid ] && rm tmp/pids/server.pid
 
 bundle exec rails db:prepare
-bundle exec rails server -p 5000 -b 0.0.0.0 &
-./bin/webpack-dev-server --hot
+bundle exec rails server -p 5000 -b 0.0.0.0
